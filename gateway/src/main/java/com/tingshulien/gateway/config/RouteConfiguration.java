a197d3e31bf4dec27feb9c11282b08138a90cb81
Tim Lien <timlientw@gmail.com>
2024-04-01 15:37:35 +0800

    Implement rate limiter for gateway server

--- gateway/src/main/java/com/tingshulien/gateway/config/RouteConfiguration.java
+++ gateway/src/main/java/com/tingshulien/gateway/config/RouteConfiguration.java
@@ -2,11 +2,14 @@ package com.tingshulien.gateway.config;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class RouteConfiguration {
@@ -38,13 +41,23 @@ public class RouteConfiguration {
             .path("/card/**")
             .filters(f -> f.rewritePath("/card/(?<segment>.*)", "/${segment}")
                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-                .retry(config -> config.setRetries(3)
-                    .setMethods(HttpMethod.GET)
-                    .setBackoff(Duration.ofMillis(200), Duration.ofMillis(1000), 2, true))
+                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                    .setKeyResolver(keyResolver()))
             )
             .uri("lb://CARD-SERVICE")
         )
         .build();
+  }
+
+  @Bean
+  public RedisRateLimiter redisRateLimiter() {
+    return new RedisRateLimiter(1, 1, 1);
+  }
+
+  @Bean
+  public KeyResolver keyResolver() {
+    return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+        .defaultIfEmpty("anonymous");
   }
 
 }
